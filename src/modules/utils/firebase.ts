b6445ead82dab3b184142d3bf91b76212970f46a
minhhung123 <prohungnguyenminh@gmail.com>
2022-08-27 17:12:11 +1000

    Add firebase utils

---
+++ src/modules/utils/firebase.ts
@@ -0,0 +1,27 @@
+import {
+    FIREBASE_API_KEY,
+    FIREBASE_AUTH_DOMAIN,
+    FIREBASE_PROJECT_ID,
+    FIREBASE_STORAGE_BUCKET,
+    FIREBASE_MESSAGING_SENDER_ID,
+    FIREBASE_APP_ID, 
+  } from '@env';
+  
+import { initializeApp, getApp, getApps } from 'firebase/app';
+import { getAuth, signInWithPhoneNumber } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
+
+const firebaseConfig = {
+    apiKey: FIREBASE_API_KEY,
+    authDomain: FIREBASE_AUTH_DOMAIN,
+    projectId: FIREBASE_PROJECT_ID,
+    storageBucket: FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: FIREBASE_MESSAGING_SENDER_ID,
+    appId: FIREBASE_APP_ID,
+}
+
+const firebaseApp = !getApps().length ? initializeApp(firebaseConfig) : getApp();
+const auth = getAuth(firebaseApp);
+const db = getFirestore(firebaseApp);
+
+export { auth, db, signInWithPhoneNumber, firebaseApp };
